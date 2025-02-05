920a85a998ed02bd8ebdbf369fdf5198208bdc32
Prince Saini <princesaini0144@gmail.com>
2025-02-06 01:16:54 +0530

    add api to call and fetch the currency optoins

--- force-app/main/default/lwc/convertCurrency/convertCurrency.js
+++ force-app/main/default/lwc/convertCurrency/convertCurrency.js
@@ -1,5 +1,53 @@
 import { LightningElement } from 'lwc';
 
 export default class ConvertCurrency extends LightningElement {
-    
+    showOutput = false;
+    convertedValue = "";
+    toCurrency = "";
+    fromCurrency = "";
+    enteredAmount = '';
+    currencyOptions = [];
+
+    connectedCallback(){
+        this.fetchSymbols();
+    }
+
+    changeHandler(event){
+        let{name, value} = event.target;
+        if(name === 'amount'){
+            this.enteredAmount = value;
+        }
+        if(name === 'fromCurr'){
+            this.fromCurrency = value;
+        }
+        if(name === 'toCurr'){
+            this.toCurrenct = value;
+        }
+
+
+    }
+
+    clickHandler(){
+
+    }
+
+    async fetchSymbols(){
+        let url = `https://api.frankfurter.dev/v1/currencies`;
+        try{
+            let response = await fetch(url);
+            if(!response.ok){
+                throw new Error('network response was not OK');
+            }
+            const data = await response.JSON();
+            let options = [];
+            for(let symbol in data){
+                options = [...options, {label: symbol, value: symbol}]
+            }
+            this.currencyOptions = [...options];
+
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
 }
