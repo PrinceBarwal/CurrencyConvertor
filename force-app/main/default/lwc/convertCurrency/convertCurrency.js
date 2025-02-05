99d6fa3d9a102002ee3b6cdd87e21ea9c907e5ba
Prince Saini <princesaini0144@gmail.com>
2025-02-06 01:48:55 +0530

    make changes in code for porper functioning

--- force-app/main/default/lwc/convertCurrency/convertCurrency.js
+++ force-app/main/default/lwc/convertCurrency/convertCurrency.js
@@ -21,29 +21,58 @@ export default class ConvertCurrency extends LightningElement {
             this.fromCurrency = value;
         }
         if(name === 'toCurr'){
-            this.toCurrenct = value;
+            this.toCurrency = value;
         }
 
 
     }
 
     clickHandler(){
-
+        console.log('Click called');
+        this.conversion();
     }
 
-    async fetchSymbols(){
-        let url = `https://api.frankfurter.dev/v1/currencies`;
+    async conversion(){
+        console.log('Inside conversion function');
+
+        let endpoint = `https://api.frankfurter.dev/v1/latest?base=${this.fromCurrency}&symbols=${this.toCurrency}`;
         try{
-            let response = await fetch(url);
+            let response = await fetch(endpoint);
+            console.log('response',response);
             if(!response.ok){
                 throw new Error('network response was not OK');
             }
-            const data = await response.JSON();
+            const data = await response.json();
+            console.log('data',data);
+            this.convertedValue = (this.enteredAmount * data.rates[this.toCurrency]).toFixed(2);
+            this.showOutput = true;
+            console.log('convertedValue',convertedValue);
+
+
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    async fetchSymbols(){
+        console.log('iside fetch method');
+        let url = `https://api.frankfurter.dev/v1/currencies`;
+        try{
+            let response = await fetch(url);
+            console.log('response',response);
+            if(!response.ok){
+                throw new Error('network response was not OK');
+            }
+            const data = await response.json();
+            console.log('data',data);
             let options = [];
             for(let symbol in data){
                 options = [...options, {label: symbol, value: symbol}]
             }
+            console.log('options',options);
             this.currencyOptions = [...options];
+            console.log('this.currencyOptions',this.currencyOptions);
 
         }
         catch(error){
